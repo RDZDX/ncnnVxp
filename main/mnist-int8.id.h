c48c5e1d6e2b2d51a3f0e8ad11019243ca635025
Audrius Puplinskas <audrius@ipk.lt>
2026-06-20 16:34:56 +0300

    Add files via upload

---
+++ main/mnist-int8.id.h
@@ -0,0 +1,17 @@
+#ifndef NCNN_INCLUDE_GUARD_mnist_int8_id_h
+#define NCNN_INCLUDE_GUARD_mnist_int8_id_h
+namespace mnist_int8_param_id {
+const int LAYER_in = 0;
+const int BLOB_data = 0;
+const int LAYER_conv0 = 1;
+const int BLOB_conv0 = 1;
+const int LAYER_pool0 = 2;
+const int BLOB_pool0 = 2;
+const int LAYER_conv1 = 3;
+const int BLOB_conv1 = 3;
+const int LAYER_pool1 = 4;
+const int BLOB_pool1 = 4;
+const int LAYER_fc = 5;
+const int BLOB_fc = 5;
+} // namespace mnist_int8_param_id
+#endif // NCNN_INCLUDE_GUARD_mnist_int8_id_h
